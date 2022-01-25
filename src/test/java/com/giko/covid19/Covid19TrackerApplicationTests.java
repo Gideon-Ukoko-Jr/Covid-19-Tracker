7f7e1a43c0c71bed8d0dff7f8f0be98b3e7db2ae
Gideon Ukoko Jr <gideon.ukoko98@gmail.com>
2022-01-25 20:52:01 +0100

    Initial commit

---
+++ src/test/java/com/giko/covid19/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giko.covid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
